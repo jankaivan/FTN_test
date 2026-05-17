956d2ecd09f63d05b9f099d137bfad763c9923a4
Janka Ivan <ivanikajanika@gmail.com>
2026-05-17 14:14:25 +0200

    Packed the functionality into a function

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 const uint16_t LED_uptime = 500;
 const uint16_t LED_downtime = 300;
 
+void ledBlink(uint16_t uptime, uint16_t lowtime)
+
 uint8_t main()
 {	
 	DDRB |= (1 << 5);
 
 	while (1)
 	{
-		PORTB |= (1 << 5);
-		_delay_ms(LED_uptime);
-		PORTB &= ~(1 << 5);
-		_delay_ms(LED_downtime);
+		ledBlink(LED_uptime, LED_downtime);
 	}
 	return 0;
+}
+
+void ledBlink(uint16_t uptime, uint16_t downtime)
+{
+	PORTB |= (1 << 5);
+	_delay_ms(uptime);
+	PORTB &= ~(1 << 5);
+	_delay_ms(downtime);
 }
