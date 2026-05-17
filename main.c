8d49a235cacffa67c877d5cc5004ef08d57a7bf7
Janka Ivan <ivanikajanika@gmail.com>
2026-05-17 14:07:19 +0200

    initial code added

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <avr/io.h>
 #include <stdint.h>
+#include <util/delay.h>
+
+const uint16_t LED_uptime = 700;
+const uint16_t LED_downtime = 500;
 
 uint8_t main()
 {	
+	DDRB |= (1 << 5);
+
+	while (1)
+	{
+		PORTB |= (1 << 5);
+		_delay_ms(LED_uptime);
+		PORTB &= ~(1 << 5);
+		_delay_ms(LED_downtime);
+	}
 	return 0;
 }
