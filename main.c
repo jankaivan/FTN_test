aa30c5d93a5b8731c6be2bfe05b49ac660148834
Janka Ivan <ivanikajanika@gmail.com>
2026-05-17 14:08:17 +0200

    changed delay values in main.c

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdint.h>
 #include <util/delay.h>
 
-const uint16_t LED_uptime = 700;
-const uint16_t LED_downtime = 500;
+const uint16_t LED_uptime = 500;
+const uint16_t LED_downtime = 300;
 
 uint8_t main()
 {	
